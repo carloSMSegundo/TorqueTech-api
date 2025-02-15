1dde23518492b844ced073f193aa864d20f52e41
Edson Alencar <edson.cesar.alencar@gmail.com>
2025-02-15 10:51:43 -0300

    fix. rename

--- src/main/java/br/com/starter/TorqueTechApiApplication.java
+++ src/main/java/br/com/starter/TorqueTechApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StarterApiApplication {
+public class TorqueTechApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StarterApiApplication.class, args);
+		SpringApplication.run(TorqueTechApiApplication.class, args);
 	}
 
 }

fd2bcf94907f048bd8b0113581d0ed627a48135b
pedinhow <phtorres5@hotmail.com>
2025-02-15 19:43:57 -0300

    feat. manager

---
+++ src/main/java/br/com/starter/domain/manager/ManagerRepository.java
@@ -0,0 +1,11 @@
+package br.com.starter.domain.manager;
+
+import br.com.starter.domain.manager.Manager;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ManagerRepository extends JpaRepository<Manager, UUID> {
+}
---
+++ src/main/resources/db/migration/V202502151938__create_manager_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE manager (
+            id UUID PRIMARY KEY NOT NULL,
+            garage_id UUID UNIQUE NOT NULL,
+            user_id UUID UNIQUE NOT NULL,
+            created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+            CONSTRAINT fk_manager_garage FOREIGN KEY (garage_id) REFERENCES garage(id) ON DELETE CASCADE,
+            CONSTRAINT fk_manager_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);

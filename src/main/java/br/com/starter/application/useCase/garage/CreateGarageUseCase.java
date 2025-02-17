57cb689d0f5737563dd064ba43c8e4ca1639c931
Edson Alencar <edson.cesar.alencar@gmail.com>
2025-02-16 21:00:25 -0300

    Fix. create garage

--- src/main/java/br/com/starter/application/useCase/garage/CreateGarageUseCase.java
+++ src/main/java/br/com/starter/application/useCase/garage/CreateGarageUseCase.java
@@ -2,11 +2,13 @@ package br.com.starter.application.useCase.garage;
 
 import br.com.starter.application.api.garage.dtos.CreateGarageDTO;
 import br.com.starter.application.api.user.dto.UserRegistrationRequest;
+import br.com.starter.domain.address.Address;
 import br.com.starter.domain.garage.Garage;
 import br.com.starter.domain.garage.GarageService;
 import br.com.starter.domain.user.UserService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -19,21 +21,22 @@ public class CreateGarageUseCase {
 
     @Transactional
     public Optional<Garage> handler(CreateGarageDTO request) {
+
+        ModelMapper mapper = new ModelMapper();
+
         var garage = new Garage();
         garage.setName(request.getName());
         garage.setCnpj(request.getCnpj());
 
-        var createUserRequest = new UserRegistrationRequest();
-        createUserRequest.setName(request.getName());
-        createUserRequest.setUsername(request.getUsername());
-        createUserRequest.setPassword(request.getPassword());
-        createUserRequest.setDocument(request.getDocument());
-        createUserRequest.setPhone(request.getPhone());
-        createUserRequest.setBirthDate(request.getBirthDate());
-        createUserRequest.setRole(request.getRole());
+        // Clona informações de endereço
+        var address = mapper.map(request.getAddress(), Address.class);
+        garage.setAddress(address);
+
+        // Clona informações de criação de usuário
+        var createUserRequest = mapper.map(request, UserRegistrationRequest.class);
+        createUserRequest.setName(request.getOwnerName()); // seta o nome certo pois a clonagem vai pegar o getName que é o nome da oficina
 
         var user = userService.create(createUserRequest);
-
         garage.setOwner(user);
 
         return Optional.of(garageService.save(garage));

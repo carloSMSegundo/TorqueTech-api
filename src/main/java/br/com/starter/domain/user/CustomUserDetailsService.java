6a22e8855ca615534371ddb5b34e0f43001f8b4d
Edson Alencar <edson.cesar.alencar@gmail.com>
2025-02-18 21:19:20 -0300

    Fix. login and get GarageByUser

--- src/main/java/br/com/starter/domain/user/CustomUserDetailsService.java
+++ src/main/java/br/com/starter/domain/user/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package br.com.starter.domain.user;
 
 import java.util.Optional;
 
+import br.com.starter.domain.garage.GarageService;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,16 +16,25 @@ import lombok.RequiredArgsConstructor;
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final GarageService garageService;
 
     @Override
     public UserDetails loadUserByUsername(String username) {
         Optional<User> user = userRepository.findByAuthUsername(username);
 
-        if (user.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário não encontrado");
+        if (user.isEmpty())
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário não encontrado");
+
+        var usetEntity = user.get();
+        var garage = garageService.getByUser(usetEntity);
+
+        if(garage.isPresent()) {
+            var garageEntity = garage.get();
+            if (garageEntity.getOwner().getStatus() == UserStatus.INACTIVE)
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário sem acesso ao sistema!");
         }
 
-        return new CustomUserDetails(user.get());
+        return new CustomUserDetails(usetEntity);
     }
 }
 

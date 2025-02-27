f9aa95642cbb72f0450a098edd86e4f689b0291f
pedinhow <phtorres5@hotmail.com>
2025-02-27 16:19:09 -0300

    feat. WorkController

--- src/main/java/br/com/starter/application/api/work/WorkController.java
+++ src/main/java/br/com/starter/application/api/work/WorkController.java
@@ -1,6 +1,14 @@
 package br.com.starter.application.api.work;
 
+import br.com.starter.application.api.common.ResponseDTO;
+import br.com.starter.application.api.work.dtos.CreateWorkRequestDTO;
+import br.com.starter.application.useCase.work.CreateWorkRequestUseCase;
+import br.com.starter.domain.user.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/torque/api/work")
 @RequiredArgsConstructor
 public class WorkController {
+
+    private final CreateWorkRequestUseCase createWorkRequestUseCase;
+
+    @PostMapping
+    public ResponseEntity<?> create(
+            @AuthenticationPrincipal CustomUserDetails userAuthentication,
+            @RequestBody CreateWorkRequestDTO request
+    ) {
+        var owner = userAuthentication.getUser();
+        return ResponseEntity.ok(
+                new ResponseDTO<>(
+                        createWorkRequestUseCase.handler(request, owner)
+                )
+        );
+    }
 }
